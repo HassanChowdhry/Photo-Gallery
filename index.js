b9e84f5c278ded27064748a209c4c5c8496decb4
Hassan Chowdhry <mohammedchowdhry11@gmail.com>
2022-01-14 09:35:55 +0500

    99% done

---
+++ index.js
@@ -0,0 +1,30 @@
+// let image = document.querySelectorAll("a");
+// let modal = document.querySelector("#myModal");
+// let span = document.getElementsByClassName("close")[0];
+
+// for(let i = 0; i <image.length; i++) {
+//   image[i].onclick = function () {
+//     modal.style.display = "block";
+// }
+// }
+
+// span.onclick = function() {
+//   modal.style.display = "none";
+// }
+
+// window.onclick = function(event) {
+//   if (event.target == modal) {
+//     modal.style.display = "none";
+//   }
+// }
+
+  let image = document.querySelectorAll("a");
+
+  for(let i = 0; i < image.length; i++) {
+  image[i].onclick = function () {
+    
+    alert("Not available!");
+   }
+  }
+  
+
